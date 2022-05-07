c9148b8937799a65022988153ade76d3094afea4
thientran98qb <thientran98qb@gmail.com>
2022-05-07 09:24:47 +0700

    install docker

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+window.axios = require("axios");
+
+import ExampleComponent from "./components/ExampleComponent.vue";
+
+new Vue({
+    el: '#app',
+    components: {
+        ExampleComponent
+    }
+});
